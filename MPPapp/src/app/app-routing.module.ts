833f541d7436cc475399ee6290d97c284647f193
Josh Gardner <josh@joshgardner.net>
2019-05-15 19:38:40 -0700

    added home components:

--- MPPapp/src/app/app-routing.module.ts
+++ MPPapp/src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { Routes } from "@angular/router";
 
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" },
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "home", component: HomeComponent },
     { path: "items", component: ItemsComponent },
+
     { path: "item/:id", component: ItemDetailComponent }
 ];
 
